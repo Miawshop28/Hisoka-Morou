7a951d832bcd77639a8fe83b29835e7fbf11730b
Miawshop28 <100006177+Miawshop28@users.noreply.github.com>
2022-06-15 20:27:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,11 +18,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','6281382420312','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
-global.author = 'WhatsApp Bot'
-global.sessionName = 'hisoka'
+global.owner = ['6283119067191']
+global.premium = ['6283119067191']
+global.packname = 'MiawBotz'
+global.author = 'MiawBotz'
+global.sessionName = 'Liaa'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
